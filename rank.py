ae6fef2212756893727487690f622e2c4b70d2af
anson20070818-dot <anson20070818@gmail.com>
2026-04-02 17:52:33 +0800

    Add files via upload

--- rank.py
+++ rank.py
@@ -1,15 +1,20 @@
-def rank(candidate_journey,mode):
-  sorted_candidate = []
-  sorted_time = []
-  sorted_cost = []
-  if mode == "cost":
-    sorted_dict = dict(sorted(candidate_journey.items(), key = lambda x: (x[1][2],x[1][1])))
-  elif mode == "time":
-    sorted_dict = dict(sorted(candidate_journey.items(), key = lambda x: (x[1][1],x[1][2])))
-  else:
-    return sorted_candidate, candidate_time, candidate_cost
-  for i in range(len(sorted_dict):
-    sorted_candidate.append(sorted_dict.values()[i][0])
-    sorted_time.append(sorted_dict.values()[[i][1])
-    sorted_cost.append(sorted_dict.values()[i][2])
-  return sorted_candidate, candidate_time, candidate_cost
+def rank(candidates, mode):
+    time, fare = 0, 0
+    duration = []
+    cost = []
+    final_candidates = []
+    for i in range(len(candidates)):
+        route = []
+        for j in range(len(candidates[i])):
+            time += candidates[i][j][1]
+            fare += candidates[i][j][2]
+            route.append((candidates[i][j][0],candidates[i][j][3]))
+        duration.append(time)
+        cost.append(fare)
+        final_candidates.append([route,duration[i],cost[i]])
+    if mode == "time":
+        final_candidates = sorted(final_candidates, key=lambda x: (x[1], x[2]))
+        return final_candidates
+    elif mode == "cost":
+        final_candidates = sorted(final_candidates, key=lambda x: (x[2], x[1]))
+        return final_candidates
