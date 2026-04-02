907be3fc39a188409efb79b39a79e22768e1468f
anson20070818-dot <anson20070818@gmail.com>
2026-04-02 17:58:37 +0800

    Added the function for candidate generation

---
+++ find_path.py
@@ -0,0 +1,14 @@
+def find_path(current, destination, candidate, candidates, network, visited):
+    if current in network:
+        if len(network[current]) >= 1:
+            for i in range (len(network[current])):
+                if network[current][i][0] == destination:
+                    candidates.append([stop for stop in candidate])
+                elif not(network[current][i][0] in visited):
+                    temp = [stop for stop in candidate]
+                    temp_visited = [stop for stop in visited]
+                    visited.append(network[current][i][0])
+                    candidate.append((network[current][i][0],network[current][i][3]))
+                    find_path(network[current][i][0], destination, candidate, candidates, network, visited)
+                    candidate = [stop for stop in temp]
+                    visited = [stop for stop in temp_visited]
