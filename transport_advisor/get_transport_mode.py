cac4cf169fcce7cc486569a671baa6460f6b4b80
anson20070818-dot <anson20070818@gmail.com>
2026-04-04 00:32:46 +0800

    Refactor loop variable for clarity in get_transport_mode

--- transport_advisor/get_transport_mode.py
+++ transport_advisor/get_transport_mode.py
@@ -1,5 +1,5 @@
 def get_transport_mode(candidate):
     transport_mode = []
-    for i in range(len(candidate)):
-        transport_mode.append(candidate[i][3])
+    for segment in range(len(candidate)):
+        transport_mode.append(candidate[segment][3])
     return transport_mode
