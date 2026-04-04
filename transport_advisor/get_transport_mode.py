1e15397e70e7f9656ed188af66acc9d9395a1490
anson20070818-dot <anson20070818@gmail.com>
2026-04-04 17:53:41 +0800

    Delete transport_advisor/get_transport_mode.py

--- transport_advisor/get_transport_mode.py
+++
@@ -1,5 +0,0 @@
-def get_transport_mode(candidate):
-    transport_mode = []
-    for segment in range(len(candidate)):
-        transport_mode.append(candidate[segment][3])
-    return transport_mode
