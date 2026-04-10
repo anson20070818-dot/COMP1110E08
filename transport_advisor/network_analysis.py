836ed75e3c0838c06bb490f4209d7dd0e6706e55
Tang Chi Hong <u3659029@connect.hku.hk>
2026-04-10 18:30:48 +0800

    Update network_analysis.py

--- transport_advisor/network_analysis.py
+++ transport_advisor/network_analysis.py
@@ -1,17 +1,16 @@
 def SummarizeNetwork(network):
     n_stops = len(network)
-    n_segments, n_bsegments, n_wsegments, n_msegments = 0, 0, 0, 0
+    n_segments = 0
+    t_segments = dict()
     for segment_list in network.values():
         for segment in segment_list:
-            if segment[3] == "Bus":
-                n_bsegments += 1
-            elif segment[3] == "Walking":
-                n_wsegments += 1
+            if t_segments.get(segment[3], -1) == -1:
+                t_segments[segment[3]] = 1
             else:
-                n_msegments += 1
+                t_segments[segment[3]] += 1
             n_segments += 1
     
-    return n_stops, n_segments, n_bsegments, n_wsegments, n_msegments
+    return n_stops, n_segments, t_segments
 
 def get_distinct_transport(network):
     transport_mode_distinct = []
