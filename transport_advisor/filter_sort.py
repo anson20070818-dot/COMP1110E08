2a6cadd01f6515255366fb39f8f6e81935ee5486
anson20070818-dot <anson20070818@gmail.com>
2026-04-10 11:20:39 +0800

    Modify filter_mode to accept dynamic transport list

--- transport_advisor/filter_sort.py
+++ transport_advisor/filter_sort.py
@@ -1,11 +1,10 @@
-def filter_mode(candidates, filter):    # 1: Bus, 2: MTR, 3: Walking, 4: no filter
-    if filter == 4:
+def filter_mode(candidates, filter, transports):    
+    if filter == len(transports):
        return candidates
     filtered_candidates = []
-    mode = ['Bus','MTR','Walking']
     for candidate in candidates:
         transport_mode = get_transport_mode(candidate)
-        if mode[filter-1] not in transport_mode:
+        if transports[filter-1] not in transport_mode:
             filtered_candidates.append(candidate)
     return filtered_candidates
 
