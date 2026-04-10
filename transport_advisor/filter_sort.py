1770972308fb5a756365da582822ff0a8713104e
anson20070818-dot <anson20070818@gmail.com>
2026-04-10 11:31:00 +0800

    Add transports parameter to filter_sort function

--- transport_advisor/filter_sort.py
+++ transport_advisor/filter_sort.py
@@ -29,8 +29,8 @@ def rank(candidates, first_preference, second_preference):    # 1:time, 2:cost, 
     return sorted(final_candidates, key = lambda x: (x[first_preference], x[second_preference], x[third_preference]))
     
 
-def filter_sort(candidates, filter, first_preference, second_preference):
-    return rank(filter_mode(candidates,filter), first_preference, second_preference)
+def filter_sort(candidates, filter, first_preference, second_preference, transports):
+    return rank(filter_mode(candidates,filter, transports), first_preference, second_preference)
 
 
 def count_transfer(transport_mode):
