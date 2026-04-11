a8e5ef329dc8e2035092cb4e25e94891bc08bffc
anson20070818-dot <anson20070818@gmail.com>
2026-04-11 21:18:03 +0800

    Refactor find_path function for clarity and efficiency

--- transport_advisor/find_path.py
+++ transport_advisor/find_path.py
@@ -1,24 +1,21 @@
 def find_path(current, destination, candidate, candidates, network, visited):
     if current in network:
-        if len(network[current]) >= 1:
-            for i in range (len(network[current])):
-                if network[current][i][0] == destination:
-                    candidate.append((network[current][i][0],network[current][i][1],network[current][i][2],network[current][i][3]))
-                    candidates.append([stop for stop in candidate])
-                elif not(network[current][i][0] in visited):
-                    temp = [stop for stop in candidate]
-                    temp_visited = [stop for stop in visited]
-                    visited.append(network[current][i][0])
-                    candidate.append((network[current][i][0],network[current][i][1],network[current][i][2],network[current][i][3]))
-                    find_path(network[current][i][0], destination, candidate, candidates, network, visited)
-                    candidate = [stop for stop in temp]
-                    visited = [stop for stop in temp_visited]
-
+        for to_stop in network[current]:
+            if to_stop[0] == destination:
+                candidate.append(tuple(to_stop))
+                candidates.append(list(candidate))
+                candidate.pop()
+            elif to_stop[0] not in visited:
+                visited.append(to_stop[0])
+                candidate.append(tuple(to_stop))
+                find_path(to_stop[0], destination, candidate, candidates, network, visited)
+                candidate.pop()
+                visited.pop()
+                    
 
 def get_candidates(origin,destination,network):
     candidates = []
-    candidate = []
+    candidate = [] 
     visited = [origin]
     find_path(origin,destination,candidate,candidates,network,visited)
     return candidates
-
