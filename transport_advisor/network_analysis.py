762857e7824c052d866e5ff7826813428e7570ca
Ken-1014 <u3659029@connect.hku.hk>
2026-04-21 17:28:48 +0800

    Added some documentation to network_analysis

--- transport_advisor/network_analysis.py
+++ transport_advisor/network_analysis.py
@@ -15,10 +15,10 @@ def SummarizeNetwork(network: dict) -> tuple[int, int, dict]:
     t_segments = dict()
     for segment_list in network.values():
         for segment in segment_list:
-            if t_segments.get(segment[3], -1) == -1:
-                t_segments[segment[3]] = 1
+            if t_segments.get(segment[3], -1) == -1:    # segment[3] is the transport mode
+                t_segments[segment[3]] = 1              # initialize in dict if not found
             else:
-                t_segments[segment[3]] += 1
+                t_segments[segment[3]] += 1             # increment count if found
             n_segments += 1
     
     return n_stops, n_segments, t_segments
@@ -28,13 +28,14 @@ def get_distinct_transport(network: dict) -> list:
     Get the dictinct transport modes for the given network.
 
     Args:
-        network (dict): A network with stops as key, a list of segments as value
+        network (dictionary): The information of segments of the network.
 
     Returns:
         list: Stores all the (distinct) transport modes of a network
+        
     """
     transport_mode_distinct = []
     for stop in network:
         for segment in network[stop]:
-          transport_mode_distinct.append(segment[3])
-    return sorted(list(set(transport_mode_distinct)))
+          transport_mode_distinct.append(segment[3])       # segment[3] is the transport mode
+    return sorted(list(set(transport_mode_distinct)))      # Remove duplicate values with set and sort to ensure order 
